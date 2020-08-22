dc1f9b31ca75e513d83e4527ca577f5110221d10
Alexander Tu <alexander.md.tu@gmail.com>
2020-08-22 10:56:30 +0200

    Initial setup for react-router example and SPA.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Home = () => (
+  <div> <h2>TKTL notes app</h2> </div>
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Notes = () => (
+  <div> <h2>Notes</h2> </div>
+)
+
+const Users = () => (
+  <div> <h2>Users</h2> </div>
+)
+
+const App = () => {
+  const [page, setPage] = useState('home')
+
+  const toPage = (page) => (event) => {
+    event.preventDefault()
+    setPage(page)
+  }
+
+  const content = () => {
+    if (page === 'home') {
+      return <Home />
+    } else if (page === 'notes') {
+      return <Notes />
+    } else if (page === 'users') {
+      return <Users />
+    }
+  }
+
+  const padding = {
+    padding: 5
+  }
+
+  return (
+    <div>
+      <div>
+        <a href="" onClick={toPage('home')} style={padding}>
+          home
+        </a>
+        <a href="" onClick={toPage('notes')} style={padding}>
+          notes
+        </a>
+        <a href="" onClick={toPage('users')} style={padding}>
+          users
+        </a>
+      </div>
+
+      {content()}
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
